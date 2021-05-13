efdde65b37be61eacd3e12fb37a8f5e392228e83
okyere <clement.okyere@gmail.com>
2021-05-13 20:03:50 +0000

    added TodoList and TodoItem

--- src/Domain/Todo/Todo.tsx
+++ src/Domain/Todo/Todo.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import {Todo } from '../../utils/types'
 
-const Todo = () => {
+let initial_todos: Todo[] = [
+    {
+    name: "make coffe",
+    completed: false,
+  },
+  {
+    name: "walk home",
+    completed: true,
+  },
+];
+
+const TodoComponent = () => {
+    let [todos, setTodos] = React.useState<Todo[]>(initial_todos);
+
+     
     return (
-        <div>
-            <h1>Welcome to Todo List</h1>
+        <div style={{ margin: "30px", padding: "20px", background: "aqua"}}>
+            <TodoList todos = {todos} />
         </div>
     )
 }
 
-export default Todo;
+type ITodoListProp = {
+    todos: Todo[]
+}
+const TodoList = ({ todos }: ITodoListProp) => {
+  console.log("Todo list props", todos);
+
+  return (
+    <div>
+      {todos.map((todo: Todo, index: any) => (
+          <TodoItem key={index} todo={todo} />
+      ))}
+    </div>
+  );
+};
+
+
+type ITodoItemProp = {
+    todo: Todo
+}
+const TodoItem = ({todo}: ITodoItemProp) => (
+    <div>
+        <span>{todo.name}</span>
+        <input type="checkbox" checked={todo.completed} />
+    </div>
+)
+
+export default TodoComponent;
