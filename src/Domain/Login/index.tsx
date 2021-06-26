4470de97cf8e0996faa39daf4b558ed4ad2fa79d
Clement Okyere <clement.okyere18@gmail.com>
2021-06-26 16:04:42 +0000

    designed log in screens

--- src/Domain/Login/index.tsx
+++ src/Domain/Login/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import './style.css';
 
 const LoginPage = () => {
    const history = useHistory();
@@ -8,12 +9,63 @@ const LoginPage = () => {
      history.push("/dashboard");
    };
 
-    return(
-    <>
-        <h1>Login in page</h1>
-        <button onClick={dashboardClickHandler}>Proceed to dashboard</button>
-        </>
-    )
+    return (
+      <div className="login_page">
+        <div className="left">
+          <div className="sub_left">
+            <div className="heading">
+              <h2>Login</h2>
+              <span className="subheading">
+                Expedite your daily planning process!
+              </span>
+            </div>
+
+            <div className="social_login">
+              <button>Sign in with Google</button>
+            </div>
+
+            <div className="email_sign_in">
+              <h4>
+                <span>or Sign in with email</span>
+              </h4>
+
+              <div className="form_group">
+                <label htmlFor="email">Email*</label>
+                <input id="email" type="email" placeholder="email" />
+              </div>
+
+              <div className="form_group">
+                <label htmlFor="password">Password*</label>
+                <input id="password" type="password" placeholder="password" />
+              </div>
+
+              <div className="miscellaneous">
+                <div className="remember_me">
+                  <input id="remember_me" type="checkbox" />
+                  <label htmlFor="remember_me">Remember me</label>
+                </div>
+                <span className="forgot_password">Forgot password?</span>
+              </div>
+
+              <div className="submit_btn">
+                <button type="submit"
+                  onClick={dashboardClickHandler}>
+                  Login
+                </button>
+              </div>
+
+              <div className="signup_info">
+                <span>
+                  Not registered yet? <a href="">Create an account</a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="right">Hello</div>
+      </div>
+    );
 };
 
 export default LoginPage;
