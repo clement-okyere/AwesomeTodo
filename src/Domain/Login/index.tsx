530be07272884e51b837646161dee53b160b36ef
Clement Okyere <clement.okyere18@gmail.com>
2021-06-29 08:10:37 +0000

    integrated formik

--- src/Domain/Login/index.tsx
+++ src/Domain/Login/index.tsx
@@ -1,6 +1,14 @@
+import { Form, Formik, FormikHelpers } from "formik";
 import React from "react";
 import { useHistory } from "react-router-dom";
+import Input from "../../Components/Input";
 import './style.css';
+
+interface Values {
+  email: string;
+  password: string;
+}
+
 
 const LoginPage = () => {
    const history = useHistory();
@@ -13,53 +21,93 @@ const LoginPage = () => {
       <div className="login_page">
         <div className="left">
           <div className="sub_left">
-            <div className="heading">
-              <h2>Login</h2>
-              <span className="subheading">
-                Expedite your daily planning process!
-              </span>
-            </div>
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              onSubmit={(
+                values: Values,
+                { setSubmitting }: FormikHelpers<Values>
+              ) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 500);
+              }}
+            >
+              {(formik) => (
+                <Form>
+                  <div className="heading">
+                    <h2>Login</h2>
+                    <span className="subheading">
+                      Expedite your daily planning!
+                    </span>
+                  </div>
 
-            <div className="social_login">
-              <button>Sign in with Google</button>
-            </div>
+                  <div className="social_login">
+                    <button>Sign in with Google</button>
+                  </div>
 
-            <div className="email_sign_in">
-              <h4>
-                <span>or Sign in with email</span>
-              </h4>
+                  <div className="email_sign_in">
+                    <h4>
+                      <span>or Sign in with email</span>
+                    </h4>
 
-              <div className="form_group">
-                <label htmlFor="email">Email*</label>
-                <input id="email" type="email" placeholder="email" />
-              </div>
+                    <Input
+                      label="Email*"
+                      type="email"
+                      id="email"
+                      placeholder="email"
+                      cssclass="form_group"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.email}
+                    />
+                    {formik.touched.email && formik.errors.email ? (
+                      <div>{formik.errors.email}</div>
+                    ) : null}
 
-              <div className="form_group">
-                <label htmlFor="password">Password*</label>
-                <input id="password" type="password" placeholder="password" />
-              </div>
+                    <Input
+                      label="Password*"
+                      type="password"
+                      id="password"
+                      placeholder="password"
+                      cssclass="form_group"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.password}
+                    />
+                    {formik.touched.password && formik.errors.password ? (
+                      <div>{formik.errors.password}</div>
+                    ) : null}
 
-              <div className="miscellaneous">
-                <div className="remember_me">
-                  <input id="remember_me" type="checkbox" />
-                  <label htmlFor="remember_me">Remember me</label>
-                </div>
-                <span className="forgot_password">Forgot password?</span>
-              </div>
+                    <div className="miscellaneous">
+                      <div className="remember_me">
+                        <input id="remember_me" type="checkbox" />
+                        <label htmlFor="remember_me">Remember me</label>
+                      </div>
+                      <span className="forgot_password">Forgot password?</span>
+                    </div>
 
-              <div className="submit_btn">
-                <button type="submit"
-                  onClick={dashboardClickHandler}>
-                  Login
-                </button>
-              </div>
+                    <div className="submit_btn">
+                      <button
+                        type="submit"
+                        // onClick={dashboardClickHandler}
+                      >
+                        Login
+                      </button>
+                    </div>
 
-              <div className="signup_info">
-                <span>
-                  Not registered yet? <a href="">Create an account</a>
-                </span>
-              </div>
-            </div>
+                    <div className="signup_info">
+                      <span>
+                        Not registered yet? <a href="#">Create an account</a>
+                      </span>
+                    </div>
+                  </div>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
 
