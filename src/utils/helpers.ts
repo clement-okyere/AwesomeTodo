5d2cbb492f5cc0c9d42ae3d167f73ae8f4c2d59c
Clement Okyere <clement.okyere18@gmail.com>
2021-07-22 15:54:57 +0000

    added getInitials

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,27 @@
-export const getToken = (name: string) => {
+export const getItem = (name: string) => {
     return localStorage.getItem(name);
 }
 
 
-export const setToken = (name: string, value: string) => {
+export const setItem = (name: string, value: string): void => {
   return localStorage.setItem(name, value);
+};
+
+export const removeItem = (name: string) => {
+  return localStorage.removeItem(name);
+};
+
+export const getInitials = (fullname: string): string => {
+  let split = fullname.split(" ");
+  let initials = "";
+  split.forEach((name: string) => {
+    initials += name[0];
+  });
+
+  return initials;
+}
+
+export const getFirstName = (fullname: string): string => {
+  let split = fullname.split(" ");
+  return split[0];
 };
