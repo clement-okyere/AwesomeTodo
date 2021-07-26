0f2ae2b91199041aeecb8fe86832bf1a610c492e
Clement Okyere <clement.okyere18@gmail.com>
2021-07-26 08:00:59 +0000

    implemented stats cards on dashboard home

--- src/Domain/Dashboard/Home/index.tsx
+++ src/Domain/Dashboard/Home/index.tsx
@@ -1,15 +1,54 @@
-import React from "react";
-import Card from "../../../Components/Card";
+import StatsCard from "../../../Components/StatsCard";
+import { faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import SectionTitle from "../../../Components/SectionTitle";
+
+
+const StyledStatsCard = styled(StatsCard)`
+ display: flex;
+ flex-direction: column;
+ width: 15em;
+`
+
+const StatsCardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  grid-column-gap: 0em;
+`;
 
 const Home = () => {
     return (
-        <>
-            <h1>Dashboard</h1>
-            <Card>
-                <span>Hello</span> 
-            </Card>
-        </>
-    )
+      <>
+        <h1>Dashboard</h1>
+        <StatsCardWrapper>
+          <StyledStatsCard
+            icon={faCalendarCheck}
+            value={1200}
+            percentageIncrease={14}
+          />
+
+          <StyledStatsCard
+            icon={faCalendarCheck}
+            value={1200}
+            percentageIncrease={14}
+          />
+
+          <StyledStatsCard
+            icon={faCalendarCheck}
+            value={1200}
+            percentageIncrease={14}
+          />
+
+          <StyledStatsCard
+            icon={faCalendarCheck}
+            value={1200}
+            percentageIncrease={14}
+          />
+            </StatsCardWrapper>
+            
+        <SectionTitle title="Recent Tasks" />
+      </>
+    );
 }
 
 export default Home;
